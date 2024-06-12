f4ca76465ef709b7bdcf8ffff3b2be42d098368a
boadabrandon <boadabrandon11@gmail.com>
2024-06-11 20:29:58 -0500

    se ha añadido un nuevo header para la vista de un usuario logueado

--- src/main/java/com/shoe/platform/controller/HomeController.java
+++ src/main/java/com/shoe/platform/controller/HomeController.java
@@ -54,8 +54,14 @@ public class HomeController {
 	
 	@GetMapping("")
 	public String home(Model model, HttpSession session) {
+		
 		log.info("Sesion del usuario: {}", session.getAttribute("idusuario"));
+		
 		model.addAttribute("productos",productoService.findAll());
+		
+		//session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		
 		return "usuario/home";
 	}
 	
@@ -129,9 +135,12 @@ public class HomeController {
 		}
 		
 		@GetMapping("/getCart")
-		public String getCart(Model model) {
+		public String getCart(Model model, HttpSession session) {
+			
 			model.addAttribute("cart", detalles);
 			model.addAttribute("orden", orden);
+			
+			model.addAttribute("sesion", session.getAttribute("idusuario"));
 			return "/usuario/carrito";
 		}
 		
