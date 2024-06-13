11f6ca11c8385098e58e1717c92a827c14f5a551
boadabrandon <boadabrandon11@gmail.com>
2024-06-13 10:19:43 -0500

    retoque algunas redirecciones en la vista del administrador

--- src/main/java/com/shoe/platform/repository/IUsuarioRepository.java
+++ src/main/java/com/shoe/platform/repository/IUsuarioRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shoe.platform.model.Usuario;
-import java.util.List;
-
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
