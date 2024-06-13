7044955ae0ad164f48ddd90a89b17248e9811f5f
boadabrandon <boadabrandon11@gmail.com>
2024-06-13 11:03:11 -0500

    Se añadio a la vista del administrador pueda ver las ordenes y los detalles de esas mismas ordenes

--- src/main/java/com/shoe/platform/controller/AdministradorController.java
+++ src/main/java/com/shoe/platform/controller/AdministradorController.java
@@ -2,25 +2,35 @@ package com.shoe.platform.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shoe.platform.model.Orden;
 import com.shoe.platform.model.Producto;
+import com.shoe.platform.service.IOrdenService;
 import com.shoe.platform.service.IUsuarioService;
 import com.shoe.platform.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
+
+	private static final Logger log = LoggerFactory.getLogger(AdministradorController.class);
 	
 	@Autowired
 	private ProductoService productoService;
 	
 	@Autowired
 	private IUsuarioService iUsuarioService;
+
+	@Autowired
+	private IOrdenService iOrdenService;
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -34,5 +44,20 @@ public class AdministradorController {
 	public String usuarios(Model model) {
 		model.addAttribute("usuarios", iUsuarioService.findAll());
 		return "administrador/usuarios";
+	}
+
+	@GetMapping("/ordenes")
+	public String ordenes(Model model){
+		model.addAttribute("ordenes", iOrdenService.findAll());
+		return "administrador/ordenes";
+	}
+
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable Integer id){
+		log.info("Id de la orden: {}",id);
+		Orden orden = iOrdenService.findById(id).get();
+
+		model.addAttribute("detalles", orden.getDetalle());
+		return "administrador/detalleorden";
 	}
 }
